5d6d59c4efe7e0305378ab850008dc0446fccbb0
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-03-19 13:24:26 +0300

    add js and extra project files

---
+++ script/order.js
@@ -0,0 +1,15 @@
+let crust = document.getElementById('crust');
+
+let toppings = document.getElementById('topping');
+
+let quantity = document.getElementById('quantity');
+
+let placeOrder = document.getElementById('placeOrders');
+
+let size = document.getElementById('size');
+
+co
+
+function myFunction(){
+    alert(size.value+ ' '+ crust.value+ ' '+ toppings.value)
+}
